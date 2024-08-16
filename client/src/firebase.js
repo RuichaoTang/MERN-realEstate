cb8c01dce4bce49fddb8549ded1fac075e2a3cbd
RuichaoTang <tangrich@outlook.com>
2024-08-15 21:08:30 -0700

    complete google authentication function

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-c1d3f.firebaseapp.com",
+  projectId: "mern-estate-c1d3f",
+  storageBucket: "mern-estate-c1d3f.appspot.com",
+  messagingSenderId: "793561953423",
+  appId: "1:793561953423:web:658b523e63a8146afaecfa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
