ef08eacab23f4c66200da069eb74a5809727b258
RuichaoTang <tangrich@outlook.com>
2024-08-16 20:00:35 -0700

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
 import User from "../models/user.model.js"
+import Listing from "../models/listing.model.js"
 
 export const test = (req,res)=> {
     res.json(
@@ -36,7 +37,19 @@ export const deleteUser = async (req, res, next)=>{
         await User.findByIdAndDelete(req.params.id)
         res.clearCookie('access_token').status(200).json('User has been deleted.')
     } catch (error) {
-        
+        next(error)
     }
     
+}
+
+export const getUserListings = async (req, res, next) =>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
+    try {  
+        console.log('here')
+
+        const listings = await Listing.find({userRef: req.params.id})
+        res.status(200).json(listings)
+    } catch (error) {
+        next(error)
+    }
 }
