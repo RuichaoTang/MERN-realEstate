19680efd2e4be3cb9883634a271e680c117abf1e
RuichaoTang <tangrich@outlook.com>
2024-08-30 00:20:54 -0700

    add onChange and onSubmit functionality to the search page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -90,7 +90,7 @@ export const searchListing = async(req, res, next) => {
 
         const searchTerm = req.query.searchTerm || ''
 
-        const sort = req.query.sort || 'createAt'
+        const sort = req.query.sort || 'createdAt'
 
         const order = req.query.order || 'desc'
 
