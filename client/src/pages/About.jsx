fee1514f9ab5644dc3e5f7aa3cc2c5b7a6417059
RuichaoTang <tangrich@outlook.com>
2024-09-04 18:08:02 -0700

    drafted the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,17 @@
  
  export default function About() {
    return (
-     <div className='pt-10 sm:pt-16'>About</div>
+     <div className='pt-10 sm:pt-16'>
+      <div className='py-20 px-4 max-w-6xl mx-auto'>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>About NestLand</h1>
+      <p>
+        Developed by Ruichao Tang, this web application practice project leverages the MERN stack, incorporating MongoDB, Express.js, React, and Node.js for full-stack development.
+      </p>
+
+     
+      </div>
+     </div>
    )
  }
  
