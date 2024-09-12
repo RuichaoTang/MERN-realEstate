8db0cc83bdc932ff07743008d5e49f53bac4fdc9
RuichaoTang <tangrich@outlook.com>
2024-09-12 15:33:45 -0700

    iphone header display issue fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
       <header className='bg-slate-200  p-1 sm:p-3 fixed top-0 left-0 right-0 z-20'>
         <div className='flex justify-between items-center max-w-lg sm:max-w-6xl mx-auto'>
             <Link to='/' title='Home' onClick={()=>{window.scrollTo(0,0)}}>
-            <h1 className='mx-3 font-bold text-xl sm:text-2xl flex flex-wrap'>
+            <h1 className='mx-3 font-bold text-base sm:text-2xl flex flex-wrap'>
                 <span className='text-slate-500'>Nest</span>
                 <span className='text-slate-700'>Land</span>
             </h1>
@@ -53,18 +53,23 @@ export default function Header() {
                 <Link to='about' onClick={() => window.scrollTo(0, 0)} title='About'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
+
+                <div className='flex flex-wrap gap-5 justify-center items-center mx-auto'>
                 <Link to='/profile' title='Profile' onClick={() => window.scrollTo(0, 0)}>
+                
+
                 {currentUser ? (
                     <img className='rounded-full h-7 w-7 sm:h-11 sm:w-11 object-cover' src={currentUser.avatar} alt="" />
-
+                    
                 ) : (<li className='text-slate-700 hover:underline'>Sign in</li>)
                 }
                 </Link>
                 <Link to={'/about'} onClick={() => window.scrollTo(0, 0)} title='About' >
                 <i className="block sm:hidden fas fa-info scale-75 hover:text-slate-400 transition-colors duration-200 text-slate-300 "></i>
                 </Link>
+                </div>
                 
-            </ul>
+                </ul>
 
         </div>
     </header>
